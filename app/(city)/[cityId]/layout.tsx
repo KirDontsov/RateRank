3dee9e701d5b71fdf560f71a6fed807bc2bedcae
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-05-02 18:26:51 +0300

    feat: add meta
    RR-999

--- app/(city)/[cityId]/layout.tsx
+++ app/(city)/[cityId]/layout.tsx
@@ -1,5 +1,5 @@
-import { HeroSection } from '@/widgets';
 import { CommonProps } from '@/shared/types';
+import { HeroSection } from '@/widgets';
 import { Metadata } from 'next/types';
 
 import { CitiesQueryResult } from '@/api';
@@ -27,6 +27,16 @@ export async function generateMetadata({ params }: CityIdProps): Promise<Metadat
   return {
     title: `Лучшие компании города ${cityName} - рейтинг кафе, баров, фастфудов, цены, фото, телефоны, адреса, отзывы — ${COMMON_TITLE}`,
     description: `Выбор лучших услуг: рестораны, салоны красоты, медицина и многое другое на ${COMMON_DOMAIN}. Фотографии, отзывы, акции, скидки, фильтры для поиска.`,
+    alternates: { canonical: `https://топвыбор.рф${city?.abbreviation}` },
+    keywords: ['отзывы', ' рейтинг', ' рестораны', ' салоны красоты', ' автосервисы', ' медицина', ` ${cityName}`],
+    openGraph: {
+      title: `Лучшие компании города ${cityName} - рейтинг кафе, баров, фастфудов, цены, фото, телефоны, адреса, отзывы — ${COMMON_TITLE}`,
+      description: `Выбор лучших услуг: рестораны, салоны красоты, медицина и многое другое на ${COMMON_DOMAIN}. Фотографии, отзывы, акции, скидки, фильтры для поиска.`,
+      url: `https://топвыбор.рф${city?.abbreviation}`,
+      siteName: `${COMMON_DOMAIN}`,
+      locale: 'ru_RU',
+      type: 'website',
+    },
     other: {
       cityId: city?.city_id ?? '',
       city: cityName ?? '',
