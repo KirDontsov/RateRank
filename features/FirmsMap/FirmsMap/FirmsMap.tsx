2ce588c942a7cb8b8aa31efd96c2df5921028aa3
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-05-06 00:33:02 +0300

    fix:  map geo-json
    RR-999

--- features/FirmsMap/FirmsMap/FirmsMap.tsx
+++ features/FirmsMap/FirmsMap/FirmsMap.tsx
@@ -1,111 +1,150 @@
 'use client';
 
-import { $category, $city, $firms, $firmsPage, Firm } from '@/api';
+import { $category, $city, $firms, $firmsForMap, $firmsPage, Firm } from '@/api';
 import { transliterate } from '@/shared';
 import { useUnit } from 'effector-react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import { MouseEvent, useRef, useState } from 'react';
+import { FC, MouseEvent, memo, useMemo, useRef, useState } from 'react';
 import Map, { FullscreenControl, GeolocateControl, Marker, NavigationControl, Popup } from 'react-map-gl';
 import styles from './map.module.scss';
+
+export interface MapItemProperties extends Firm {
+  longitude: string;
+  latitude: string;
+}
+
+export interface MapItem {
+  id: number;
+  properties: MapItemProperties;
+}
+
+export interface MarkersComponentProps {
+  items: MapItem[] | null;
+  zoomToSelectedLoc: (e: MouseEvent<HTMLButtonElement>, item: MapItem) => void;
+}
+
+/** Работаем с гео-json и itemsInViewPort иначе тормозит */
+const MarkersComponent: FC<MarkersComponentProps> = ({ zoomToSelectedLoc, items }) => {
+  const markerRef = useRef<mapboxgl.Marker>(null);
+
+  return (
+    <>
+      {!!items?.length &&
+        items?.map((item, index) => {
+          return (
+            <Marker
+              ref={markerRef}
+              key={index}
+              longitude={Number(item?.properties?.longitude ?? 0)}
+              latitude={Number(item?.properties?.latitude ?? 0)}
+              pitchAlignment="viewport"
+            >
+              <button type="button" className="cursor-pointer" onClick={(e) => zoomToSelectedLoc(e, item)}>
+                <div className={styles.marker} />
+              </button>
+            </Marker>
+          );
+        })}
+    </>
+  );
+};
+
+export const Markers = memo(MarkersComponent);
 
 export const FirmsMap = () => {
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
   const searchParams = useSearchParams();
-  const [selectedMarker, setSelectedMarker] = useState<{ firm: Firm } | null>(null);
+  const [selectedMarker, setSelectedMarker] = useState<MapItem | null>(null);
+  const [itemsInViewPort, setItemsInViewPort] = useState<MapItem[] | null>(null);
   const mapRef = useRef(null);
 
   const { firms, city, category, page } = useUnit({
-    firms: $firms,
+    firms: $firmsForMap,
     city: $city,
     category: $category,
     page: $firmsPage,
   });
 
-  const zoomToSelectedLoc = (e: MouseEvent<HTMLButtonElement>, firm: Firm) => {
+  console.log('itemsInViewPort', itemsInViewPort);
+
+  const zoomToSelectedLoc = (e: MouseEvent<HTMLButtonElement>, item: MapItem) => {
     // stop event bubble-up which triggers unnecessary events
     e.stopPropagation();
-    const coords = firm?.coords?.split(', ');
 
-    setSelectedMarker({ firm });
+    setSelectedMarker(item);
     // @ts-ignore
-    mapRef.current?.flyTo({ center: [coords?.[0] ?? 0, coords?.[1] ?? 0], zoom: 17 });
+    mapRef.current?.flyTo({ center: [item?.properties?.longitude ?? 0, item?.properties?.latitude ?? 0], zoom: 17 });
   };
 
   const initialCoords = city?.coords?.split(', ');
 
   return (
     <div className={styles.mainStyle}>
-      <Map
-        ref={mapRef}
-        mapboxAccessToken={mapboxToken}
-        mapStyle="mapbox://styles/kirdontsov/ck6cbhtnf27vu1inwoo33gdcx"
-        // @ts-ignore
-        className={styles.mapStyle}
-        style={{ width: '100%', height: '100svh' }}
-        initialViewState={{
-          longitude: Number(initialCoords?.[0] ?? 0),
-          latitude: Number(initialCoords?.[1] ?? 0),
-          zoom: 10,
-        }}
-        maxZoom={20}
-        minZoom={1}
-        pitch={45}
-        bearing={-17.6}
-      >
-        {firms?.map((firm, index) => {
-          const coords = firm?.coords?.split(', ');
+      {!!firms?.length && (
+        <Map
+          ref={mapRef}
+          mapboxAccessToken={mapboxToken}
+          mapStyle="mapbox://styles/kirdontsov/ck6cbhtnf27vu1inwoo33gdcx"
+          // @ts-ignore
+          className={styles.mapStyle}
+          style={{ width: '100%', height: '100svh' }}
+          initialViewState={{
+            longitude: Number(initialCoords?.[0] ?? 0),
+            latitude: Number(initialCoords?.[1] ?? 0),
+            zoom: 10,
+          }}
+          maxZoom={20}
+          minZoom={1}
+          pitch={45}
+          bearing={-17.6}
+          onMove={() => {
+            const featuresInViewport = mapRef?.current
+              // @ts-ignore
+              ?.getMap()
+              .queryRenderedFeatures({ layers: ['car-services-msk'] });
+            if (featuresInViewport?.length < 300) {
+              setItemsInViewPort(featuresInViewport);
+            }
+          }}
+        >
+          <Markers items={itemsInViewPort} zoomToSelectedLoc={zoomToSelectedLoc} />
 
-          return (
-            <Marker key={index} longitude={Number(coords?.[0] ?? 0)} latitude={Number(coords?.[1] ?? 0)}>
-              <button type="button" className="cursor-pointer" onClick={(e) => zoomToSelectedLoc(e, firm)}>
-                <div className={styles.marker} />
-              </button>
-            </Marker>
-          );
-        })}
+          {selectedMarker ? (
+            <Popup
+              latitude={Number(selectedMarker?.properties?.latitude ?? 0)}
+              longitude={Number(selectedMarker?.properties?.longitude ?? 0)}
+              onClose={() => {
+                setSelectedMarker(null);
+              }}
+              closeButton={true}
+              closeOnClick={true}
+              className={`${styles.popupMain} text-gray-900 dark:text-white`}
+              anchor="bottom-left"
+            >
+              <h3 className={styles.popupTitle}>{selectedMarker?.properties?.name}</h3>
+              <div className={`${styles.popupInfo} text-gray-900 dark:text-white dark:bg-gray-800`}>
+                <label className={`${styles.popupLabel} text-blue-400 dark:text-blue-400`}>Адрес: </label>
+                <p>{selectedMarker?.properties?.address}</p>
+                <br />
 
-        {selectedMarker ? (
-          <Popup
-            latitude={Number(selectedMarker?.firm?.coords?.split(', ')[0] ?? 0)}
-            longitude={Number(selectedMarker?.firm?.coords?.split(', ')[0] ?? 0)}
-            onClose={() => {
-              setSelectedMarker(null);
-            }}
-            closeButton={true}
-            closeOnClick={true}
-            className={`${styles.popupMain} text-gray-900 dark:text-white`}
-            anchor="bottom-left"
-          >
-            <h3 className={styles.popupTitle}>{selectedMarker?.firm?.name}</h3>
-            <div className={`${styles.popupInfo} text-gray-900 dark:text-white dark:bg-gray-800`}>
-              <label className={`${styles.popupLabel} text-blue-400 dark:text-blue-400`}>Адрес: </label>
-              <p>{selectedMarker?.firm?.address}</p>
-              <br />
-              {selectedMarker?.firm?.default_phone && (
-                <>
-                  <label className={`${styles.popupLabel} text-blue-400 dark:text-blue-400`}>Тел: </label>
-                  <p>{selectedMarker?.firm?.default_phone}</p>
-                  <br />
-                </>
-              )}
+                <Link
+                  href={`/${city?.abbreviation}/${category?.abbreviation}/${selectedMarker?.properties?.url || transliterate(selectedMarker?.properties?.name ?? '')}?firmsPage=${Number(searchParams.get('firmsPage')) || page}`}
+                  // target={selectedMarker?.firm?.url === '' ? null : '_blank'}
+                  className={styles.popupWebUrl}
+                >
+                  Подробнее
+                </Link>
+              </div>
+            </Popup>
+          ) : null}
 
-              <Link
-                href={`/${city?.abbreviation}/${category?.abbreviation}/${selectedMarker?.firm?.url || transliterate(selectedMarker?.firm?.name ?? '')}?firmsPage=${Number(searchParams.get('firmsPage')) || page}`}
-                // target={selectedMarker?.firm?.url === '' ? null : '_blank'}
-                className={styles.popupWebUrl}
-              >
-                Подробнее
-              </Link>
-            </div>
-          </Popup>
-        ) : null}
-
-        <GeolocateControl position="top-left" style={{ marginTop: '154%' }} />
-        <NavigationControl position="top-left" />
-        <FullscreenControl position="bottom-right" />
-      </Map>
+          <GeolocateControl position="top-left" style={{ marginTop: '154%' }} />
+          <NavigationControl position="top-left" />
+          <FullscreenControl position="bottom-right" />
+        </Map>
+      )}
     </div>
   );
 };
