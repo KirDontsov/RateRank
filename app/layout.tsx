763e4f2cc77775469fd924577d65f2d2a298070c
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-05-21 09:33:37 +0300

    feat: add webmaster waiter
    TV-999

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,8 +46,9 @@ export default function RootLayout({ children }: CommonProps) {
         <CookiesProvider>{children}</CookiesProvider>
         <ToastContainer />
         {process.env.PRODUCTION && (
-          <Script id="metrika-counter" strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          <>
+            <Script id="metrika-counter" strategy="afterInteractive">
+              {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 		        m[i].l=1*new Date();
 		        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
 		        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -59,7 +60,18 @@ export default function RootLayout({ children }: CommonProps) {
 		             accurateTrackBounce:true,
 		             webvisor:true
 		        });`}
-          </Script>
+            </Script>
+            <Script id="webmaster-waiter" strategy="afterInteractive">
+              {`window.YandexRotorSettings = {
+                  WaiterEnabled: true;
+                  FailOnTimeout: false;
+                  NoJsRedirectsToMain: true
+                  IsLoaded: function() {
+                      return document.body.querySelectorAll('div').length > 10;
+                  }
+              }`}
+            </Script>
+          </>
         )}
       </body>
     </html>
