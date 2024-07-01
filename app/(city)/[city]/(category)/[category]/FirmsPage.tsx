330a6668c18eee98d715ae8565e0b79d1f232022
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-07-01 18:41:33 +0300

    fix:  styles
    TV-999

--- app/(city)/[city]/(category)/[category]/FirmsPage.tsx
+++ app/(city)/[city]/(category)/[category]/FirmsPage.tsx
@@ -90,7 +90,7 @@ export const FirmsPage: FC<FirmsPageProps & CommonNavProps> = ({
                         </div>
                       </div>
                     </div>
-                    <div className="flex flex-col 2xl:w-[79%] xl:w-[59%] md:w-full w-full">
+                    <div className="flex flex-col 2xl:w-[79%] xl:w-[59%] md:w-full w-full px-4 md:px-0">
                       <FirmsMap />
                     </div>
                   </div>
