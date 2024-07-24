4c67ecb770771f400f0a54c3cad1d4b957375884
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-07-24 16:10:04 +0300

    fix: images
    RR-001

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,7 +1,3 @@
-import car_services_placeholder from '../../public/images/car_services_placeholder.webp';
-import clubs_placeholder from '../../public/images/clubs_placeholder.webp';
-import restaurants_placeholder from '../../public/images/restaurants_placeholder.webp';
-
 export const FETCH_LIMIT = 10;
 
 export const BACKEND_PORT = 'https://xn--90ab9accji9e.xn--p1ai';
@@ -11,9 +7,9 @@ export const DEFAULT_PHOTOS_ENDPOINT = 'https://xn--n1aalg.xn--90ab9accji9e.xn--
 export const DEFAULT_PHOTOS_EXT = 'webp';
 
 export const HeroBackground = {
-  '3ebc7206-6fed-4ea7-a000-27a74e867c9a': restaurants_placeholder,
-  '565ad1cb-b891-4185-ac75-24ab3898cf22': car_services_placeholder,
-  'cc1492f6-a484-4c5f-b570-9bd3ec793613': clubs_placeholder,
+  '3ebc7206-6fed-4ea7-a000-27a74e867c9a': `${DEFAULT_PHOTOS_ENDPOINT}/placeholders/restaurants_placeholder.${DEFAULT_PHOTOS_EXT}`,
+  '565ad1cb-b891-4185-ac75-24ab3898cf22': `${DEFAULT_PHOTOS_ENDPOINT}/placeholders/car_services_placeholder.${DEFAULT_PHOTOS_EXT}`,
+  'cc1492f6-a484-4c5f-b570-9bd3ec793613': `${DEFAULT_PHOTOS_ENDPOINT}/placeholders/clubs_placeholder.${DEFAULT_PHOTOS_EXT}`,
 };
 
 export const COMMON_TITLE = 'Топ выбор';
