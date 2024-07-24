688dd72d931576806b91155887cd50872f47169b
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-07-24 15:57:08 +0300

    fix: images
    RR-001

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,3 +1,7 @@
+import car_services_placeholder from '../../public/images/car_services_placeholder.webp';
+import clubs_placeholder from '../../public/images/clubs_placeholder.webp';
+import restaurants_placeholder from '../../public/images/restaurants_placeholder.webp';
+
 export const FETCH_LIMIT = 10;
 
 export const BACKEND_PORT = 'https://xn--90ab9accji9e.xn--p1ai';
@@ -7,12 +11,9 @@ export const DEFAULT_PHOTOS_ENDPOINT = 'https://xn--n1aalg.xn--90ab9accji9e.xn--
 export const DEFAULT_PHOTOS_EXT = 'webp';
 
 export const HeroBackground = {
-  '3ebc7206-6fed-4ea7-a000-27a74e867c9a':
-    'https://images.unsplash.com/photo-1550966871-3ed3cdb5ed0c?q=80&w=5070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  '565ad1cb-b891-4185-ac75-24ab3898cf22':
-    'https://images.unsplash.com/photo-1486496572940-2bb2341fdbdf?q=80&w=5070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'cc1492f6-a484-4c5f-b570-9bd3ec793613':
-    'https://images.unsplash.com/photo-1593958812614-2db6a598c71c?q=80&w=5070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  '3ebc7206-6fed-4ea7-a000-27a74e867c9a': restaurants_placeholder,
+  '565ad1cb-b891-4185-ac75-24ab3898cf22': car_services_placeholder,
+  'cc1492f6-a484-4c5f-b570-9bd3ec793613': clubs_placeholder,
 };
 
 export const COMMON_TITLE = 'Топ выбор';
