816ce56491a34f4ddf30383cebc53987653ed6bb
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-10-11 16:07:38 +0300

    fix: image by url
    RR-001

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,7 +1,7 @@
 export const FETCH_LIMIT = 10;
 
-// export const BACKEND_PORT = 'https://xn--90ab9accji9e.xn--p1ai';
-export const BACKEND_PORT = 'http://localhost:8080';
+export const BACKEND_PORT = 'https://xn--90ab9accji9e.xn--p1ai';
+// export const BACKEND_PORT = 'http://localhost:8080';
 
 export const DEFAULT_PHOTOS_ENDPOINT = 'https://xn--n1aalg.xn--90ab9accji9e.xn--p1ai';
 export const DEFAULT_PHOTOS_EXT = 'webp';
